a3e35350bb50e6874cb5f546810d54abf4d3232f
dmtz <diego_tuason@dlsu.edu.ph>
2021-09-21 18:55:27 +0800

    Charts work

--- index.js
+++ index.js
@@ -22,21 +22,8 @@ var currRegion = 0;
 
 const phGini = 0.4267;
 const phMeanFamily = 4.4930;
-
-let food = 0;
-let clothes = 0;
-let hh = 0;
-let health = 0;
-let fuel = 0;
-let comm = 0;
-let trans = 0;
-let educ = 0;
-let rec =0;
-let misc =0;
-let furni = 0;
-let rent = 0;
-let occa = 0;
-let other = 0;
+//adjust chart css here
+const width = 800, height = 400, margin = {t:10,b:30,l:200,r:10};
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/asdfgeist/ckti3tekg2oo918wb39r6w2o1',
@@ -53,6 +40,18 @@ var currFormatter = Intl.NumberFormat({
     maximumFractionDigits:2
 })
 
+var dataRegion = [{key: 'Food', value: 10},{key: 'Clothes', value: 10},
+           {key: 'Household', value: 10},{key: 'Health', value: 10},
+           {key: 'Fuel', value: 10},{key: 'Communication', value: 10},
+           {key: 'Transportation', value: 10},{key: 'Education', value: 10},
+           {key: 'Recreation', value: 10},{key: 'Miscellany', value: 10},
+           {key: 'Furniture', value: 10},{key: 'Rent', value: 10},
+            {key: 'Occasions', value: 10},{key: 'Other', value: 10}];
+
+var dataRegionInc = [{key: 'Regular Salary', value: 10},{key: 'Seasonal Salary', value: 10},
+           {key: 'Entrepreneurial Income', value: 10},{key: 'Other Sources of Income', value: 10},
+           {key: 'Cash/Support From Domestic Source', value: 10},
+           {key: 'Cash/Support From Abroad', value: 10}];    
 map.on('load', () =>{
     map.getCanvas().style.cursor = '';
     map.addSource('phregions', {
@@ -109,6 +108,8 @@ map.on('load', () =>{
                         "<br>Respondents: "+e.features[0].properties['count'])
             .addTo(map);*/
 
+        // datasets
+        // chart dimensions 
         /*
         if (phGini > parseFloat(e.features[0].properties['gini'])){
             document.getElementById("gini-national-comparison").innerHTML =  "<b class='text-success'>" + (phGini - parseFloat(e.features[0].properties['gini'])).toFixed(4) +"</b>"+ " More than the national index."
@@ -143,6 +144,13 @@ map.on('load', () =>{
             rent = parseFloat(e.features[0].properties['mean_rent_exp']);
             occa = parseFloat(e.features[0].properties['mean_occasions_exp']);
             other = parseFloat(e.features[0].properties['mean_others_exp']);
+        
+            reg = parseFloat(e.features[0].properties['mean_reg_salary']);
+            entrep = parseFloat(e.features[0].properties['mean_entrep_inc']);
+            season = parseFloat(e.features[0].properties['mean_seasonal_salary']);
+            otherInc = parseFloat(e.features[0].properties['mean_others_salary']);
+            domes = parseFloat(e.features[0].properties['cash_domestic']);
+            abroad = parseFloat(e.features[0].properties['cash_abroad']);
 
           let dataUpdate = [{key: 'Food', value: food},{key: 'Clothes', value: clothes},
                        {key: 'Household', value: hh},{key: 'Health', value: health},
@@ -151,8 +159,43 @@ map.on('load', () =>{
                        {key: 'Recreation', value: rec},{key: 'Miscellany', value: misc},
                        {key: 'Furniture', value: furni},{key: 'Rent', value: rent},
                         {key: 'Occasions', value: occa},{key: 'Other', value: other}];
+<<<<<<< Updated upstream:index.js
             console.log('qwe')
             updateData(dataUpdate);
+=======
+
+          let dataUpdateIncome = [{key: 'Regular Salary', value: reg},
+                                  {key: 'Seasonal Salary', value: season},
+                                  {key: 'Entrepreneurial Income', value: entrep},
+                                  {key: 'Other Sources of Income', value: otherInc},
+                                  {key: 'Cash/Support From Domestic Source', value: domes},
+                                  {key: 'Cash/Support From Abroad', value: abroad}];
+        
+        updateData(dataUpdate,chart2,xAxis2,yAxis2);
+        updateData(dataUpdateIncome,chart4,xAxis4,yAxis4);
+        if (selectRegion !== null) {
+            /*clear*/
+            map.setFilter('selected', ['==', ['get', 'REGION'],null]);
+        }
+        selectRegion = e.features[0].properties['REGION'];
+
+        map.setFilter('selected', ['==', ['get', 'REGION'], e.features[0].properties['REGION']])
+        selectRegion = null;
+
+        if (phMeanFamily > parseFloat(e.features[0].properties['mean_family_size'])){
+            document.getElementById("famsize-comparison").innerHTML =  "<em><small><b class='text-success'>" + (phMeanFamily - parseFloat(e.features[0].properties['mean_family_size'])).toFixed(2) +"</b>"+ " more than the <span><abbr title='4.493'>nat'l average</span>.</small></em>"
+        } else{
+            document.getElementById("famsize-comparison").innerHTML = "<em><small><b class='text-danger'>" + (parseFloat(e.features[0].properties['mean_family_size']) - phMeanFamily).toFixed(2) +"</b>"+ " less than the <span><abbr title='4.493'>nat'l average</span>.</small></em>"
+        }
+
+        if (phGini > parseFloat(e.features[0].properties['gini'])){
+            document.getElementById("gini-comparison").innerHTML =  "<em><small><b class='text-danger'>" + (phGini - parseFloat(e.features[0].properties['gini'])).toFixed(4) +"</b>"+ " more than the nat'l index.</small></em>"
+        } else{
+            document.getElementById("gini-comparison").innerHTML = "<em><small><b class='text-success'>" + (parseFloat(e.features[0].properties['gini']) - phGini).toFixed(4) +"</b>"+ " less than the national index.</small></em>"
+        }
+
+        
+>>>>>>> Stashed changes:public/index.js
     });
     map.on('mousemove', 'fies', (e) => {
         map.getCanvas().style.cursor = 'pointer';
@@ -178,6 +221,7 @@ map.on('load', () =>{
         }
         currRegion = null;
     });
+<<<<<<< Updated upstream:index.js
     
   // datasets
   let dataRegion = [{key: 'Food', value: 0},{key: 'Clothes', value: 0},
@@ -197,65 +241,102 @@ map.on('load', () =>{
       .attr('height', height)
       .style('border', 'solid 1px #222');
     let chart = svg.append('g')
-        .classed('chart', true)
-        .attr('transform', `translate(${margin.l},${margin.t})`);
-          // axes areas
-    let xAxis = svg.append('g')
-        .classed('axis', true)
-        .attr('transform', `translate(${margin.l},${height-margin.b})`);
-    let yAxis = svg.append('g')
-        .classed('axis', true)
-        .attr('transform', `translate(${margin.l},${margin.t})`);
-    
+=======
+    // svg element
     let svg2 = d3.select('div#barContainer')
       .append('svg')
       .attr('width', width)
       .attr('height', height)
-      .style('border', 'solid 1px #222');
-    
-    let chart2 = svg2.append('g')
+      .style('border', 'solid 1px #fff');
+
+    var chart2 = svg2.append('g')
+>>>>>>> Stashed changes:public/index.js
         .classed('chart', true)
         .attr('transform', `translate(${margin.l},${margin.t})`);
           // axes areas
-    let xAxis2 = svg2.append('g')
+    var xAxis2 = svg2.append('g')
         .classed('axis', true)
         .attr('transform', `translate(${margin.l},${height-margin.b})`);
-    let yAxis2 = svg2.append('g')
+    var yAxis2 = svg2.append('g')
         .classed('axis', true)
         .attr('transform', `translate(${margin.l},${margin.t})`);
-    updateData(dataRegion);
+
+
+    var svg4 = d3.select('div#barContainer3')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+<<<<<<< Updated upstream:index.js
+      .style('border', 'solid 1px #222');
+=======
+      .style('border', 'solid 1px #fff');
+
+    var chart4 = svg4.append('g')
+        .classed('chart', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+          // axes areas
+    var xAxis4 = svg4.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${height-margin.b})`);
+    var yAxis4 = svg4.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+
+    updateData(dataRegion,chart2,xAxis2,yAxis2);
+    updateData(dataRegionInc,chart4,xAxis4,yAxis4);
+>>>>>>> Stashed changes:public/index.js
+    
+    
+
+
+    var svg = d3.select('div#barContainer2')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .style('border', 'solid 1px #fff');
+    var chart = svg.append('g')
+        .classed('chart', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+          // axes areas
+    var xAxis = svg.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${height-margin.b})`);
+    var yAxis = svg.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+
+
+
+    var svg3 = d3.select('div#barContainer4')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .style('border', 'solid 1px #fff');
+
+    var chart3 = svg3.append('g')
+        .classed('chart', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+          // axes areas
+    var xAxis3 = svg3.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${height-margin.b})`);
+    var yAxis3 = svg3.append('g')
+        .classed('axis', true)
+        .attr('transform', `translate(${margin.l},${margin.t})`);
+
   var data = d3.csv("https://raw.githubusercontent.com/carlraymundo/DATA101-Final-Project/diego/data/aggdata.csv?token=AGC4UAD46DLLI2NWNAIN3S3BJH24G",
   function(d) {
     return {
       region: d.REGION,
       mean_expenditure: d.mean_expenditure,
-      mean_family_size: d.mean_family_size,
-      mean_income: d.mean_income,
-      mean_reg_salary: d.mean_reg_salary,
-      mean_seasonal_salary: d.mean_seasonal_salary,
-      mean_entrep_inc: d.mean_entrep_inc,
-      mean_others_salary: d.mean_others_salary,
-      cash_abroad: d.cash_abroad,
-      cash_domestic: d.cash_domestic,
-      mean_food_exp: d.mean_food_exp,
-      mean_clothes_exp: d.mean_clothes_exp,
-      mean_furnishings_exp: d.mean_furnishings_exp,
-      mean_health_exp: d.mean_health_exp,
-      mean_fuels_exp: d.mean_fuels_exp,
-      mean_rent_exp: d.mean_rent_exp,
-      mean_transpo_exp: d.mean_transpo_exp,
-      mean_comms_exp: d.mean_comms_exp,
-      mean_recs_exp: d.mean_recs_exp,
-      mean_educ_exp: d.mean_educ_exp,
-      mean_misc_exp: d.mean_misc_exp,
-      mean_furniture_exps: d.mean_furniture_exps,
-      mean_occasions_exp: d.mean_occasions_exp,
-      mean_others_exp: d.mean_others_exp,
-      count: d.count
+      mean_income: d.mean_income
     };
   }).then(function(dataset) {
       // transitions
+<<<<<<< Updated upstream:index.js
         console.log(dataset);
+=======
+>>>>>>> Stashed changes:public/index.js
       let d = 500;
       let tRemove = d3.transition()
           .duration(d);
@@ -314,11 +395,81 @@ map.on('load', () =>{
       setTimeout(()=>{bars.merge(barsEnter).classed('new', false)}, d*4)
   });
     
+  var dataIncome = d3.csv("https://raw.githubusercontent.com/carlraymundo/DATA101-Final-Project/diego/data/aggdata.csv?token=AGC4UAD46DLLI2NWNAIN3S3BJH24G",
+  function(d) {
+    return {
+      region: d.REGION,
+      mean_income: d.mean_income
+    };
+  }).then(function(dataset) {
+      // transitions
+      let d = 500;
+      let tRemove = d3.transition()
+          .duration(d);
+      let tPosition = d3.transition()
+          .duration(d)
+          .delay(d);
+      let tSize = d3.transition()
+          .duration(d)
+          .delay(d*2);
+      
+      let regions = dataset.map(function(d) {return d.region; });
+      // scales
+      //console.log(d3.max(dataset, function(d) { return d.mean_income; }));
+      let xScale = d3.scaleLinear()
+          .domain([0, d3.max(dataset, d=>d.mean_income)])
+          .range([0, width-margin.l-margin.r]);
+      let yScale = d3.scaleBand()
+          .domain(regions)
+          .range([0, height-margin.t-margin.b])
+          .padding(0.2);
+      // axes
+      d3.axisBottom(xScale)(xAxis3.transition(tSize));
+      d3.axisLeft(yScale)(yAxis3.transition(tPosition));
+      // update pattern
+      // initial selection
+      bars = chart3.selectAll('rect.bar');
+
+      // data binding
+      bars = bars.data(dataset, d=>d.region);
+      // exit selection
+      bars.exit()
+          .classed('obs', true)
+          .transition(tRemove)
+          .attr('width', 0)
+          .remove();
+
+      // enter selection
+      let barsEnter = bars.enter().append('rect')
+          .classed('bar new', true)
+          .attr('x', xScale(0))
+          .on('mouseover', function(e,d){
+              d3.select(this).classed('highlight', true);
+          })
+          .on('mouseout', function(e,d){
+              d3.select(this).classed('highlight', false);
+          });
+      // update selection
+      bars.classed('new', false);
+      // enter + update selection
+
+    //   var color = d3.scaleSequential().domain([0, maxRatio]).interpolator(d3.interpolateGreens);
+
+      bars.merge(barsEnter)
+          .transition(tPosition)
+          .attr('y', d=>yScale(d.region))
+          .attr('height', yScale.bandwidth())
+          .transition(tSize)
+          .attr('width', d=>xScale(d.mean_income))
+          .style("fill", d => region_color(d.region));
+      // class reset
+      setTimeout(()=>{bars.merge(barsEnter).classed('new', false)}, d*4)
+  });
   // chart area
   // update function
-  function updateData(dataset){
+  function updateData(dataset,chartF,xAxisF,yAxisF){
       // transitions
-        console.log('qwe');
+      console.log(chartF);
       let d = 500;
       let tRemove = d3.transition()
           .duration(d);
@@ -331,8 +482,12 @@ map.on('load', () =>{
       
       let categs = dataset.map(function(d) {return d.key; });
       // scales
+<<<<<<< Updated upstream:index.js
       console.log(d3.max(dataset, function(d) { return d.value; }));
       console.log(d.value);
+=======
+    //   let exp_color = d3.scaleSequential().domain([0, d3.max(dataset, d=>d.value)]).interpolator(d3.interpolateYlGn);
+>>>>>>> Stashed changes:public/index.js
       let xScale = d3.scaleLinear()
           .domain([0, d3.max(dataset, d=>d.value)])
           .range([0, width-margin.l-margin.r]);
@@ -341,11 +496,11 @@ map.on('load', () =>{
           .range([0, height-margin.t-margin.b])
           .padding(0.2);
       // axes
-      d3.axisBottom(xScale)(xAxis2.transition(tSize));
-      d3.axisLeft(yScale)(yAxis2.transition(tPosition));
+      d3.axisBottom(xScale)(xAxisF.transition(tSize));
+      d3.axisLeft(yScale)(yAxisF.transition(tPosition));
       // update pattern
       // initial selection
-      bars = chart2.selectAll('rect.bar');
+      bars = chartF.selectAll('rect.bar');
       // data binding
       bars = bars.data(dataset, d=>d.key);
       // exit selection
